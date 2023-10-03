1a113c3146c8215e3bb9a06c125bbecd77a8c996
harshsingh262 <harshsingh84562@gmail.com>
2023-10-03 20:12:23 +0530

    Add file Day3

---
+++ Day3.py
@@ -0,0 +1,12 @@
+def insertionSort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        key=arr[i]
+        j=i-1
+        while j>=0 and key<arr[j]:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=key
+arr=[85,45,100,-9,5,2,10.250]
+insertionSort(arr)
+print(arr)
