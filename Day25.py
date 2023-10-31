bc5a6be4310f10944567e1bd14c7106a317b949b
harshsingh262 <harshsingh84562@gmail.com>
2023-10-31 22:03:13 +0530

    Using ThreadPool

---
+++ Day25.py
@@ -0,0 +1,9 @@
+import concurrent.futures
+def worker():
+  print("Worker thread running")
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+pool.submit(worker)
+pool.submit(worker)
+pool.submit(worker)
+pool.shutdown(wait = True)
+print("Main Thread continuing to run")
